0994cf6d803069209194e665953c27f9bba32aeb
YinCheng <eason010630@gmail.com>
2023-05-09 01:21:23 +0800

    0.0.3

--- src/components/componentA.vue
+++ src/components/componentA.vue
@@ -1,13 +1,13 @@
 <template>
-    <p>Hello World!</p>
+    <p>vue-vite 練習</p>
     {{data}}
     <input type="text" v-model='text'/>
-    <p>{{text}}</p>
-</template>
+    <p>輸出：{{text}}</p>
+</template>｀
 
 <script setup>
 import {ref} from '@vue/reactivity'
-    const data = 'hello vue'
+    const data = '輸入：'
     const text = ref('123')
 </script>
 
